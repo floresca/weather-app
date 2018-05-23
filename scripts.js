222f252aad5c15887a1dd992a1fe6b0ba628d9d7
Andres <floresc.andres@intracitygeeks.org>
2018-05-22 20:14:51 -0400

    Background image auto change

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     var latitude;
     var longitude;
-    
+     
     
 // The following is the code for the day of the week and date
     $("#day-date").ready(function upDate() {
@@ -58,34 +58,56 @@ $(document).ready(function() {
     });
     
     
-// The f
-            
-            
-            
+// The following code calls the
     
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
             longitude = position.coords.longitude;
             latitude = position.coords.latitude;
-        var callingWeather = "https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude;
-// The following is the code for weather
-
-        $.getJSON(callingWeather, weatherStuff);
-        
-        function weatherStuff(weatherData){
-            var Temp = weatherData.main.temp;
-            var NAME = weatherData.name;
-            var status = weatherData.weather[0].main;
-            var icon = weatherData.weather[0].icon;
             
-           $(".temperature").append(Math.round((Temp * (9/5)) +32) + '&#8457;');
-           $(".location").append(NAME);
-           $(".status").append(status);
-           $(".icon").attr("src",icon);
-        }
-    });
-}
+// The following code calls the weather API using the latitude and Longitude
 
+        var callingWeather = "https://fcc-weather-api.glitch.me/api/current?lat="+latitude+"&lon="+longitude;
+        
+        $.getJSON(callingWeather, weatherStuff); //this is the API caller (address, and function to be run)
+        
+            function weatherStuff(weatherData){
+                var Temp = weatherData.main.temp;
+                var NAME = weatherData.name;
+                var status = weatherData.weather[0].main;
+                var Conditions = weatherData.weather[0].id;
+        
+        // After we call the address and get its values we output those values to the HTML
+                $(".temperature").append(Math.round((Temp * (9/5)) +32) + '&#8457;');
+                $(".location").append(NAME);
+                $(".status").append(status);
+
+//Here is the code switching out the background image depending on the ID values inside status
+               
+                if (Conditions >= 200 && Conditions < 300){
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/Thunder.jpg)"});
+                } else if (Conditions >= 300 && Conditions < 400) {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/lightRain.jpg)"});
+                } else if (Conditions >= 500 && Conditions < 600) {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/Rainy.jpeg)"});
+                } else if (Conditions == 600 || Conditions == 601 && Conditions >= 611 && Conditions < 700) {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/Snow.jpg)"});
+                } else if (Conditions == 602) {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/snowSpecial.jpg)"});
+                } else if (Conditions >= 700 && Conditions < 800) {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/fog.jpg)"});
+                } else if (Conditions >= 801 && Conditions < 805) {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/Cloudy.jpg)"});
+                } else {
+                    $("body").css({"background":"url(../WeatherApp/weatherImage/Sunny.jpg)"});
+                }
+            }
+            
+        });
+    }
+
+
+    
         
     
     
